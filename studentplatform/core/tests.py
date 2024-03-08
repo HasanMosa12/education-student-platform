224a7a1d5e30454cf9a8313a544a2a82f6603153
hmosa <hasanmo@ac.sce.ac.il>
2024-03-08 13:43:47 +0200

    the signup of the parent

--- studentplatform/core/tests.py
+++ studentplatform/core/tests.py
@@ -5,9 +5,9 @@ from django.test import Client
 
 class YourTestClass(TestCase):
     def test_invalid_signup_student_form(self):
-        data = {'username': ''}  # Missing required field
+        data = {'username': ''}
         response = self.client.post(reverse('signup_student'), data)
-        self.assertEqual(response.status_code, 400)  # Use 400 for a bad request
+        self.assertEqual(response.status_code, 400)
         self.assertTrue(response.context['form'].has_errors())
         self.assertEqual(response.context['form']['username'].errors, ['This field is required.'])
 
@@ -21,13 +21,13 @@ class YourTestClass(TestCase):
         }
         response = self.client.post(reverse('signup_student'), data)
         self.assertEqual(User.objects.count(), 1)
-        self.assertRedirects(response, expected_url=reverse('some_next_page'))  # Adjust the expected URL accordingly
+        self.assertRedirects(response, expected_url=reverse('some_next_page'))
 
     def test_signup_with_incomplete_data(self):
-        data = {'username': 'newstudent'}  # Missing 'email' and 'password'
+        data = {'username': 'newstudent'}
         response = self.client.post(reverse('signup_student'), data)
         self.assertEqual(User.objects.count(), 0)
-        self.assertEqual(response.status_code, 400)  # Use 400 for a bad request
+        self.assertEqual(response.status_code, 400)
         self.assertTrue('form' in response.context)
         self.assertFalse(response.context['form'].is_valid())
 
@@ -40,5 +40,5 @@ class YourTestClass(TestCase):
             'password2': 'newpassword123',
         }
         response = self.client.post(reverse('signup_student'), data)
-        self.assertEqual(User.objects.count(), 1)  # No new user should be added
-        self.assertTrue(response.context['form'].errors)  # Check for errors in the form
+        self.assertEqual(User.objects.count(), 1)
+        self.assertTrue(response.context['form'].errors)
